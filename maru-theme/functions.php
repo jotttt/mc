a24f72c4ff1357ed439334c9938aa5c47592d07e
jotttt <jlaulik@gmail.com>
2017-11-13 16:26:00 +0200

    13.11

--- maru-theme/functions.php
+++ maru-theme/functions.php
@@ -8,78 +8,78 @@
  */
 
 if ( ! function_exists( 'maru_theme_setup' ) ) :
-	/**
+/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
 	 * Note that this function is hooked into the after_setup_theme hook, which
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function maru_theme_setup() {
-		/*
+function maru_theme_setup() {
+    /*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on Maru Theme, use a find and replace
 		 * to change 'maru-theme' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'maru-theme', get_template_directory() . '/languages' );
+    load_theme_textdomain( 'maru-theme', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support( 'automatic-feed-links' );
 
-		/*
+    /*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+    add_theme_support( 'title-tag' );
 
-		/*
+    /*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'post-thumbnails' );
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'maru-theme' ),
-		) );
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'menu-1' => esc_html__( 'Primary', 'maru-theme' ),
+    ) );
 
-		/*
+    /*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
 		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+    add_theme_support( 'html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ) );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'maru_theme_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
+    // Set up the WordPress core custom background feature.
+    add_theme_support( 'custom-background', apply_filters( 'maru_theme_custom_background_args', array(
+        'default-color' => 'ffffff',
+        'default-image' => '',
+    ) ) );
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+    // Add theme support for selective refresh for widgets.
+    add_theme_support( 'customize-selective-refresh-widgets' );
 
-		/**
+    /**
 		 * Add support for core custom logo.
 		 *
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
+    add_theme_support( 'custom-logo', array(
+        'height'      => 250,
+        'width'       => 250,
+        'flex-width'  => true,
+        'flex-height' => true,
+    ) );
+}
 endif;
 add_action( 'after_setup_theme', 'maru_theme_setup' );
 
@@ -91,7 +91,7 @@ add_action( 'after_setup_theme', 'maru_theme_setup' );
  * @global int $content_width
  */
 function maru_theme_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'maru_theme_content_width', 640 );
+    $GLOBALS['content_width'] = apply_filters( 'maru_theme_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'maru_theme_content_width', 0 );
 
@@ -101,15 +101,15 @@ add_action( 'after_setup_theme', 'maru_theme_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function maru_theme_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'maru-theme' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'maru-theme' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+    register_sidebar( array(
+        'name'          => esc_html__( 'Sidebar', 'maru-theme' ),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__( 'Add widgets here.', 'maru-theme' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
 }
 add_action( 'widgets_init', 'maru_theme_widgets_init' );
 
@@ -117,7 +117,10 @@ add_action( 'widgets_init', 'maru_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function maru_theme_scripts() {
-	wp_enqueue_style( 'maru-theme-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'maru-theme-style', get_stylesheet_uri() );
+
+    wp_enqueue_style( 'maru-theme-bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '20151215', 'all' );
+
 
     wp_enqueue_script( 'maru-theme-jquery', "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js", array(), '3.2.1', true );
 
@@ -127,13 +130,13 @@ function maru_theme_scripts() {
 
     wp_enqueue_script( 'maru-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'maru-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'maru-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
     wp_enqueue_script( 'maru-theme-bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), '20151215', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'maru_theme_scripts' );
 
@@ -161,6 +164,6 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+    require get_template_directory() . '/inc/jetpack.php';
 }
 
